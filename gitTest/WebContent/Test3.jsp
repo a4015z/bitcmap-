c4b1d8ba8c817fe472e9db0ebd782003df889908
Jong <a4015z@naver.com>
2019-01-11 16:44:42 +0900

    pull 을 생활화 하자

---
+++ gitTest/WebContent/Test3.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+올라가는거 맞니ssss바꼇다
+추가되었다.
+</body>
+</html>
--- homework/src/homework/JDBCExample1.java
+++ homework/src/homework/JDBCExample1.java
@@ -4,25 +4,27 @@ import java.sql.*;
 
 public class JDBCExample1 {
 	public static void main(String[] args) {
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-			String user ="student";
-			String pw ="1234";
-			//2 .데이터베이스 연결 성공 
-			Connection connection = DriverManager.getConnection(url);
+
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "student";
+			String pw = "1234";
+			// 2 .데이터베이스 연결 성공
+			Connection conn = DriverManager.getConnection(url, user, pw);
 			System.out.println("데이터 베이스 연결 성공");
-			
-		// 3. 데이터 베이스 작업 
-			
-			
-			//4 .데이터 연결 종료 
-		
-			connection.close();
-			
+
+			// 3. 데이터 베이스 작업
+
+			// 4 .데이터 연결 종료
+
+			conn.close();
+
 		} catch (ClassNotFoundException cnfe) {
-			System.out.println("해당 클래스를 찾을 수 없습니다." + cnfe.getMessage());
+			System.out.println("해당클래스를 찾을수 없습니다.");
+			System.out.println();
+
 		} catch (SQLException se) {
 			System.out.println(se.getMessage());
 		}
